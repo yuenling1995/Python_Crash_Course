a435f2d9d3cf3727511e1902947f41629e4e6ba4
yuenling1995 <45129170+yuenling1995@users.noreply.github.com>
2022-02-24 14:34:24 -0500

    Add files via upload

---
+++ chapter1_2/famous_quote.py
@@ -0,0 +1,6 @@
+name = "albert einstein"
+quote = '"A person who never made a mistake never tried anything new."'
+sentence = name.title() + ' once said, ' + quote 
+print(sentence)
+
+
---
+++ chapter1_2/strip_names.py
@@ -0,0 +1,10 @@
+name = " \tYuen\n "
+
+#print the var as it is
+print(name)
+#strip off the left spacing in the var
+print(name.lstrip())
+#strip off the right spacing in the var
+print(name.rstrip())
+#strip off both sides spacing in the var
+print(name.strip())
