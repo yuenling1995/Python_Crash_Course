81d4011c1c77885cc1e4d8dc80b2db088fbc0875
yuenling1995 <45129170+yuenling1995@users.noreply.github.com>
2022-03-23 12:39:54 -0400

    Create addition.py

---
+++ chapter10/addition.py
@@ -0,0 +1,19 @@
+print("Give me two numbers, and I will add them up: ")
+print("Enter 'q' to quit: ")
+
+while True:
+	first_num = input("\nFirst number: ")
+	if first_num == 'q':
+		break 
+
+	second_num = input("\nSecond number: ")
+	if second_num == 'q':
+		break
+
+	try:
+		answer = int(first_num) + int(second_num)
+	except ValueError:
+		print("\nEither input value is not a number! ")
+		pass
+	else:
+		print("The sum is " + str(answer))
